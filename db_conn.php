329c106af6b995730f01d16bf46e77e8dc51e2ef
VictorCR27 <danielsegurajaime@gmail.com>
2023-06-07 16:54:57 -0600

    cambios string db

--- db_conn.php
+++ db_conn.php
@@ -1,8 +1,8 @@
 <?
    $servername = "192.185.77.249";
-   $username = "visualcr_user";
+   $username = "visualcr_dwUser";
    $password = "Uni2023op&";
-   $database = "visualcr_db";
+   $database = "visualcr_dw";
 
    //String conex
    $conex = mysqli_connect($servername, $username, $password, $database);
