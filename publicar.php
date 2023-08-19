7a2919a742471af3360bb69d59595545c320a4d6
Victor Segura <danielsegurajaime@gmail.com>
2023-08-19 02:01:33 -0600

    cambios publciar

--- publicar.php
+++ publicar.php
@@ -15,6 +15,8 @@ if (!$conex) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>Publicar</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <style>
         .form-publicar{
             margin-top: 100px;
@@ -33,7 +35,6 @@ if (!$conex) {
                 echo '<a href="ver_publicaciones.php">Ver publicaciones</a>';
             }
             ?>
-            <a href="#">Reservar</a>
             <a href="#">Servicios</a>
             <a href="#">Quienes somos?</a>
             <a href="#">Cuenta</a>
@@ -58,8 +59,24 @@ if (!$conex) {
             <label for="nombre_hotel">Nombre del Hotel:</label>
             <input type="text" name="nombre_hotel"><br><br>
 
-            <label for="amenidades">Amenidades:</label>
-            <textarea name="amenidades"></textarea><br><br>
+            <label for="amenidad1">Amenidad 1:</label>
+            <input type="text" name="amenidad1"><br><br>
+
+            <label for="amenidad2">Amenidad 2:</label>
+            <input type="text" name="amenidad2"><br><br>
+
+            <label for="amenidad3">Amenidad 3:</label>
+            <input type="text" name="amenidad3"><br><br>
+
+            <label for="amenidad4">Amenidad 4:</label>
+            <input type="text" name="amenidad4"><br><br>
+
+            <label for="amenidad5">Amenidad 5:</label>
+            <input type="text" name="amenidad5"><br><br>
+
+            <label for="amenidad6">Amenidad 6:</label>
+            <input type="text" name="amenidad6"><br><br>
+
 
             <label for="pais">País:</label>
             <select name="pais" id="pais">
@@ -73,7 +90,12 @@ if (!$conex) {
             </select><br><br>
 
             <label for="ubicacion">Ubicación:</label>
-            <input type="text" name="ubicacion"><br><br>
+            <input type="text" id="ubicacion-input" name="ubicacion" placeholder="Busca una ubicación en el mapa"><br><br>
+            <div id="map" style="height: 500px; width: 800px;"></div>
+            <input type="hidden" id="coordenadas" name="coordenadas">
+
+            
+
 
             <label for="imagenes">Imágenes:</label>
             <input type="file" name="imagenes[]" multiple><br><br>
@@ -116,6 +138,37 @@ if (!$conex) {
         }
     }
     ?>
+    
+    <script>
+    // Inicializar el mapa centrado en Centroamérica
+    var map = L.map('map').setView([14.1, -89.4], 6);
+
+    // Agregar el mapa base (puedes elegir un proveedor de mapas)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Manejar la selección de ubicación
+   // Manejar la selección de ubicación
+    map.on('click', function(e) {
+        const coordenadasInput = document.getElementById('coordenadas');
+        const ubicacionInput = document.getElementById('ubicacion-input');
+        
+        coordenadasInput.value = JSON.stringify({
+            lat: e.latlng.lat,
+            lng: e.latlng.lng
+        });
+
+        // Reverse geocoding to get the place name based on coordinates
+        fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${e.latlng.lat}&lon=${e.latlng.lng}`)
+            .then(response => response.json())
+            .then(data => {
+                const placeName = data.display_name;
+                ubicacionInput.value = placeName;
+            });
+    });
+    </script>
+
 
 </body>
 </html>
