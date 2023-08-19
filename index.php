4c2a8ef0124845610fc551cc4fb56ca47ee870ab
adrianl27 <alopezv188@ulacit.ed.cr>
2023-08-18 23:33:03 -0600

    Cambios en main page
    Se agregaron los reviews y le di estilo a los cards

--- index.php
+++ index.php
@@ -218,6 +218,117 @@ include("login.php");
                 </div>
                 <!--Fin Cards-->
 
+    <div class="reviews-completo">
+        <h2>Reseñas</h2>
+        <div class="carousel" data-carousel>
+        <div class="carousel-buttons">
+          <button
+            class="carousel-button carousel-button_previous"
+            data-carousel-button-previous
+          >
+            <span class="fas fa-chevron-circle-left"></span>
+          </button>
+          <button
+            class="carousel-button carousel-button_next"
+            data-carousel-button-next
+          >
+            <span class="fas fa-chevron-circle-right"></span>
+          </button>
+        </div>
+        <div class="slides" data-carousel-slides-container>
+          <div class="slide">
+          <div class="reviews">
+             <div class="reviews-contenedor">     
+                 <div class="reviews-icono">
+                   <img src="imgCarrusel/avatar-hombre.png">
+                 </div>
+                     <div class="reviews-contenido">
+                       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti magnam recusandae velit nemo facere aut minus
+                           perspiciatis odit possimus at?</h4>
+                       <h6>Nombre</h6>
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+                          <path
+                            d="m337-225 143-85 143 86-39-161 126-110-166-14-64-152-64 152-166 13 126 109-39 162Zm143 24L292-87q-14 8-28 7t-25-9q-11-8-16-20.5t-2-28.5l50-214-166-145q-12-10-15.5-23.5T90-547q3-13 14.5-22t27.5-10l219-19 85-203q6-15 18.5-22t25.5-7q13 0 25.5 7t18.5 22l85 203 220 19q15 1 26.5 10t14.5 22q4 13 .5 26.5T855-497L689-352l50 214q3 16-2 28.5T721-89q-11 8-25 9t-28-7L480-201Zm0-232Z" />
+                        </svg> </div>
+             </div>
+            </div>
+          </div>
+          <div class="slide">
+          <div class="reviews">
+             <div class="reviews-contenedor">     
+                 <div class="reviews-icono">
+                   <img src="imgCarrusel/avatar-mujer.png">
+                 </div>
+                     <div class="reviews-contenido">
+                       <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti magnam recusandae velit nemo facere aut minus
+                           perspiciatis odit possimus at?</h4>
+                       <h6>Nombre</h6>
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                         <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+                          <path
+                            d="m337-225 143-85 143 86-39-161 126-110-166-14-64-152-64 152-166 13 126 109-39 162Zm143 24L292-87q-14 8-28 7t-25-9q-11-8-16-20.5t-2-28.5l50-214-166-145q-12-10-15.5-23.5T90-547q3-13 14.5-22t27.5-10l219-19 85-203q6-15 18.5-22t25.5-7q13 0 25.5 7t18.5 22l85 203 220 19q15 1 26.5 10t14.5 22q4 13 .5 26.5T855-497L689-352l50 214q3 16-2 28.5T721-89q-11 8-25 9t-28-7L480-201Zm0-232Z" />
+                        </svg>
+                     </div>
+             </div>
+         </div>
+          </div>
+          <div class="slide">
+          <div class="reviews">
+            <div class="reviews-contenedor">     
+                <div class="reviews-icono">
+                  <img src="imgCarrusel/avatar-hombre2.png">
+                </div>
+                    <div class="reviews-contenido">
+                      <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti magnam recusandae velit nemo facere aut minus
+                          perspiciatis odit possimus at?</h4>
+                      <h6>Nombre</h6>
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+                          <path
+                            d="m337-225 143-85 143 86-39-161 126-110-166-14-64-152-64 152-166 13 126 109-39 162Zm143 24L292-87q-14 8-28 7t-25-9q-11-8-16-20.5t-2-28.5l50-214-166-145q-12-10-15.5-23.5T90-547q3-13 14.5-22t27.5-10l219-19 85-203q6-15 18.5-22t25.5-7q13 0 25.5 7t18.5 22l85 203 220 19q15 1 26.5 10t14.5 22q4 13 .5 26.5T855-497L689-352l50 214q3 16-2 28.5T721-89q-11 8-25 9t-28-7L480-201Zm0-232Z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="slide">
+          <div class="reviews">
+            <div class="reviews-contenedor">     
+                <div class="reviews-icono">
+                  <img src="imgCarrusel/avatar-mujer2.png">
+                </div>
+                    <div class="reviews-contenido">
+                      <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti magnam recusandae velit nemo facere aut minus
+                          perspiciatis odit possimus at?</h4>
+                      <h6>Nombre</h6>
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <img src="imgCarrusel/star_FILL1_wght700_GRAD0_opsz48.svg#ebd725">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="35" viewBox="0 -960 960 960" width="35">
+                          <path
+                            d="m337-225 143-85 143 86-39-161 126-110-166-14-64-152-64 152-166 13 126 109-39 162Zm143 24L292-87q-14 8-28 7t-25-9q-11-8-16-20.5t-2-28.5l50-214-166-145q-12-10-15.5-23.5T90-547q3-13 14.5-22t27.5-10l219-19 85-203q6-15 18.5-22t25.5-7q13 0 25.5 7t18.5 22l85 203 220 19q15 1 26.5 10t14.5 22q4 13 .5 26.5T855-497L689-352l50 214q3 16-2 28.5T721-89q-11 8-25 9t-28-7L480-201Zm0-232Z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    
+
                 <!--Mapa-->
                 <div class="mapa" style="text-align: center;">
                     <h2 style="text-align: left; padding-left: 35px;font-family: 'Jost', sans-serif; color: rgb(23, 146, 29);">Mapa de Centroamérica</h2>
@@ -239,10 +350,11 @@ include("login.php");
         </div>
         
     <!--Scripts-->
-    <script src="script.js"></script>
+    <script src="js/script.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <!--Fin Scripts-->
+    <script src="https://kit.fontawesome.com/c14ef371b4.js" crossorigin="anonymous"></script>
+    <!--Fin Scripts-->   
 </body>
 
 </html>
