d071f20da3077d400d096421acdac98ff1b4dbea
Victor Segura <danielsegurajaime@gmail.com>
2023-08-21 18:04:39 -0600

    cambios

--- reservar.php
+++ reservar.php
@@ -29,7 +29,6 @@ $cantidadHabitaciones = isset($_GET['cantidadHabitaciones']) ? $_GET['cantidadHa
     <title>Reservar</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="css/otrosEstilos.css">
 </head>
 <body>
